d51546ad1e744b2a118f804be3e826cc1b2b0256
fjodor-rybakov <fedryb@mail.ru>
2019-04-25 21:00:36 +0300

    add base field

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public Transform cell;
+    public Transform cellsParent;
+    private Transform[,] sprites;
+
+    private float x = -1.85f;
+    private float y = 1.75f;
+
+    void Awake()
+    {
+        InitField();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void InitField()
+    {
+        int arrColl = 0, arrCell = 0;
+        sprites = new Transform[4, 4];
+        for (int i = 1; i <= 16; i++)
+        {
+            Transform tempObj = (Transform)Instantiate(cell, new Vector3(x, y, 0), Quaternion.identity);
+            sprites[arrColl, arrCell] = tempObj;
+            tempObj.transform.SetParent(cellsParent.transform);
+            tempObj.GetComponent<Cell>().posCell = arrCell;
+            tempObj.GetComponent<Cell>().posColl = arrColl;
+
+            arrColl++;
+            x += 1.85f;
+
+            if (i % 4 == 0)
+            {
+                arrColl = 0;
+                arrCell++;
+                x = -1.85f;
+                y -= 1.85f;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
